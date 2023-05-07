747ef72d2bd4f96ea5666f20f1973e031423f627
zynkd <coding707@mailbox.org>
2023-05-07 14:59:25 +0200

    Created `GithubContext` and moved all current states to it

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -7,7 +7,7 @@ function UserItem({ user: { login, avatar_url } }) {
       <div className='flex-row items-center space-x-4 card-body'>
         <div>
           <div className='avatar'>
-            <div className='rounded-full shadow w-14 h-14'>
+            <div className='rounded-full w-14 h-14'>
               <img src={avatar_url} alt='profile' />
             </div>
           </div>
