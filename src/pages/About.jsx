d06b3f6db763002684b714ee4526e23a9863f8c6
zynkd <coding707@mailbox.org>
2023-05-07 10:40:04 +0200

    Created pages and added Routes to them

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-5xl mb-6'>Github Finder</h1>
+
+      <p className='mb-4 text-xl font-light'>
+        This is a React application that allows users to search Github profiles,
+        retrieve their profile data via Github API, and display them in a nice
+        way.
+      </p>
+    </>
+  );
+}
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+function Home() {
+  return (
+    <div>
+      <h1 className="text-5xl">Welcome Home</h1>
+    </div>
+  )
+}
+export default Home
