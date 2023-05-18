e275c7f34d96734d63a33f34519c1ff9e3e2bd1b
zynkd <coding707@mailbox.org>
2023-05-18 19:02:38 +0200

    Change BrowserRouter to HashRouter to deploy easier to droplet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
